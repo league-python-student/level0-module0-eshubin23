e11a9926657983a20a05bd2e351cdc7444a2fdb0
league <gmjangwoo0827@gmail.com>
2023-10-20 18:26:17 -0700

    I worked on cat and high low

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -44,10 +44,10 @@ def key_pressed():
     # when you press the space bar. Run the program to test it.
 
     # 10. Increment the x and y variables of the 2 eye variables by 5:
-    #     left_eye.x += 5
-    
+    left_eye.x += 5
+    right_eye.x += 5
     # 11. Call the .draw() method for both eye variables.
-
+    right_eye.draw()
 
 if __name__ == '__main__':
     window = turtle.Screen()
@@ -58,33 +58,38 @@ if __name__ == '__main__':
     #    c. Rename the image something short (e.g. cat.gif)
     #    d. Save the image to your computer's desktop
     #    e. Drag and drop the image into this python package
-    
+    bg_image = 'bigEyedCat2.gif'
     # 2. Call the set_background() function with your variable inside of the parenthesis
-    #    for example, set_background(bg_image)
-
+    #    for example, set_background(https://github.com/league-python-student/level0-module0-eshubin23.gitbg_image)
+    set_background(bg_image)
     # 3. Make a new turtle
-
+    hi=turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-
+    hi.color('red', 'red')
     # 5. Set the turtle width to 0 so no outlines are drawn
-
+    hi.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-
+    hi.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your
     #    processing window.
     #    Variables for x and y have been created at the top of your sketch, 
     #    now you can set them equal to the values you just found. Watch for
     #    negative signs!
-    
+    x= -53.0
+    y=65.0
+
+    x2=7.0
+    y2=58.8
+
     # 8. After you've found the x and y for the eyes create 2 eye variables
     #    and initialize them:
-    #    left_eye  = Eye(eye=my_turtle, x=-34, y=11, radius=30)
-    #    right_eye = Eye(eye=my_turtle, x=40, y=-5, radius=30)
+    left_eye  = Eye(eye=hi, x=x, y=y, radius=30)
+    right_eye = Eye(eye=hi, x=x2, y=y2, radius=30)
 
     # 9. Call the .draw() method on BOTH eye variables
-
+    left_eye.draw()
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     window.withdraw()
 
     # 1. Change this line to give you a random number between 1 - 100.
-    random_num = random.randint(1, 10)
+    random_num = random.randint(1, 100)
 
     # 2. Print out the random variable that you made in step #1
-
+    print(random_num)
     # 3. Code a for loop to run steps 4-10, 10 times
-
+    for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-
+        answer = simpledialog.askstring(title='answer', prompt='Guess a number between 1 - 100')
         # 5. If the guess is correct
             # 6. Win. Use 'sys.exit(0)' to end the program
 
