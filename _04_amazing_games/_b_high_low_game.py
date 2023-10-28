97992752847e0bf04d50b6616dfa3577c8b0c5fb
league <gmjangwoo0827@gmail.com>
2023-10-27 17:36:30 -0700

    I worked on cat and high low

--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -14,15 +14,20 @@ if __name__ == '__main__':
     # 3. Code a for loop to run steps 4-10, 10 times
     for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-        answer = simpledialog.askstring(title='answer', prompt='Guess a number between 1 - 100')
+        answer = simpledialog.askinteger(title='answer', prompt='Guess a number between 1 - 100')
         # 5. If the guess is correct
+        if answer == random_num:
             # 6. Win. Use 'sys.exit(0)' to end the program
-
+            'sys.exit(0)'
         # 7. if the guess is high
+        if answer > random_num:
+
             # 8. Tell them it's too high
+            messagebox.showinfo("your answer is to high")
         # 9. Else if the guess is low
+        else:
             # 10. Tell them it's too low
-
+            messagebox.showinfo("your answer is to low")
     #11. Outside of the loop, tell the user they lost
-
+    messagebox.showinfo("you lost")
     window.mainloop()
